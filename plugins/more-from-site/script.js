7971ae634111bcb51842564a3390e0ba62daf5e6
Kristian Skov <kris701kj@gmail.com>
2026-06-17 17:56:56 +0200

    Domain blocking integration
    Allows the "More from site" button to be grouped under the three dots when having domain blocking enabled

--- plugins/more-from-site/script.js
+++ plugins/more-from-site/script.js
@@ -1,8 +1,21 @@
+
+const onShowMoreClick = (s) => {
+	const searchBar = document.getElementById("results-search-input")
+	if (!searchBar) return;
+	
+	var targetUrl = s.srcElement.getAttribute("targetUrl")
+	var toAdd = " site:" + targetUrl;
+	if (!searchBar.value.endsWith(toAdd)){
+		const searchButton = document.getElementById("results-search-btn")
+		if (!searchButton) return;
+		searchBar.value += toAdd
+		searchButton.dispatchEvent(new Event('click', { bubbles: true }));
+	}
+}
+
 const addButtons = () => {
 	const searchBar = document.getElementById("results-search-input")
 	if (!searchBar) return;
-	const searchButton = document.getElementById("results-search-btn")
-	if (!searchButton) return;
 	
 	const results = document.querySelectorAll(".result-url-row")
 	for(var result of results){
@@ -10,22 +23,32 @@ const addButtons = () => {
 		if (!existing){
 			var url = result.querySelector(".result-cite");
 			if (!url) continue;
-			let newItem = document.createElement("p");
+			var targetUrl = url.innerHTML
+			targetUrl = targetUrl.substring(0,targetUrl.indexOf('/'))
+			if (searchBar.value.endsWith(" site:" + targetUrl)) continue;
+			
 			let text = document.createTextNode("More from site");
-			newItem.appendChild(text);
-			newItem.className = 'more-from-site'
-			newItem.setAttribute("targetUrl", url.innerHTML)
-			newItem.addEventListener("click", (s) => {
-				var targetUrl = s.srcElement.getAttribute("targetUrl")
-				targetUrl = targetUrl.substring(0,targetUrl.indexOf('/'))
-				var toAdd = " site:" + targetUrl;
-				if (!searchBar.value.endsWith(toAdd)){
-					searchBar.value += toAdd
-					searchButton.dispatchEvent(new Event('click', { bubbles: true }));
-				}
-				
-			})
-			result.appendChild(newItem);
+			
+			var dropdown = result.querySelector(".result-actions-menu")
+			if (dropdown)
+			{
+				let newItem = document.createElement("button");
+				newItem.appendChild(text);
+				newItem.className = 'more-from-site result-actions-item degoog-menu-item'
+				newItem.setAttribute("targetUrl", targetUrl)
+				newItem.setAttribute("type", "button")
+				newItem.setAttribute("role", "menuitem")
+				newItem.addEventListener("click", onShowMoreClick)
+				dropdown.appendChild(newItem);
+			}
+			else {
+				let newItem = document.createElement("p");
+				newItem.appendChild(text);
+				newItem.className = 'more-from-site more-from-site-single'
+				newItem.setAttribute("targetUrl", targetUrl)
+				newItem.addEventListener("click", onShowMoreClick)
+				result.appendChild(newItem);
+			}
 		}
 	}
 }
