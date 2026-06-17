703e27a194a867119d1eb463c37d9b217c91b1fe
Kristian Skov <kris701kj@gmail.com>
2026-06-17 17:34:09 +0200

    Minor fix

--- plugins/more-from-site/index.js
+++ plugins/more-from-site/index.js
@@ -1,6 +1,15 @@
+const INFO_HTML =
+  `<div class="command-result"><p>Adds an option to show more results from a single site.</p></div>`;
+
 export default {
   isClientExposed: false,
   name: "More From Site",
   description: "Adds an option to show more results from a single site.",
-  settingsSchema: []
+  settingsSchema: [],
+  trigger: "more",
+  aliases: ["m"],
+
+  execute() {
+    return { title: "More from site", html: INFO_HTML };
+  },
 };
