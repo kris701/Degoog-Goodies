d307fa27bfdd3f5caa4d8c80fa8dc4bbd097fb54
Kristian Skov <kris701kj@gmail.com>
2026-06-17 17:23:31 +0200

    Settings cleanup

--- plugins/more-from-site/index.js
+++ plugins/more-from-site/index.js
@@ -1,15 +1,6 @@
-const INFO_HTML =
-  `<div class="command-result"><p>Test.</p></div>`;
-
 export default {
   isClientExposed: false,
   name: "More From Site",
   description: "Adds an option to show more results from a single site.",
-  trigger: "highlight",
-  aliases: ["hl"],
-  settingsSchema: [],
-
-  execute() {
-    return { title: "More From Site", html: INFO_HTML };
-  },
+  settingsSchema: []
 };
